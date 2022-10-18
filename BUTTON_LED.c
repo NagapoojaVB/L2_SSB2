e8aae54c66255c76614d20b8db3961ff9357b0d4
NagapoojaVB <114560181+NagapoojaVB@users.noreply.github.com>
2022-10-18 17:13:03 +0530

    Add files via upload

---
+++ BUTTON_LED.c
@@ -0,0 +1,120 @@
+/* ###################################################################
+**     Filename    : ProcessorExpert.c
+**     Project     : ProcessorExpert
+**     Processor   : MKL25Z128VLK4
+**     Version     : Driver 01.01
+**     Compiler    : GNU C Compiler
+**     Date/Time   : 2022-10-11, 11:57, # CodeGen: 0
+**     Abstract    :
+**         Main module.
+**         This module contains user's application code.
+**     Settings    :
+**     Contents    :
+**         No public methods
+**
+** ###################################################################*/
+/*!
+** @file ProcessorExpert.c
+** @version 01.01
+** @brief
+**         Main module.
+**         This module contains user's application code.
+*/         
+/*!
+**  @addtogroup ProcessorExpert_module ProcessorExpert module documentation
+**  @{
+*/         
+/* MODULE ProcessorExpert */
+
+
+/* Including needed modules to compile this module/procedure */
+#include "Cpu.h"
+#include "Events.h"
+#include "Bit1.h"
+#include "BitIoLdd1.h"
+#include "Bit2.h"
+#include "BitIoLdd2.h"
+#include "Byte1.h"
+#include "BitsIoLdd1.h"
+#include "Byte2.h"
+#include "BitsIoLdd2.h"
+/* Including shared modules, which are used for whole project */
+#include "PE_Types.h"
+#include "PE_Error.h"
+#include "PE_Const.h"
+#include "IO_Map.h"
+
+/* User includes (#include below this line is not maintained by Processor Expert) */
+void print(int a,int b);
+/*lint -save  -e970 Disable MISRA rule (6.3) checking. */
+int main(void)
+/*lint -restore Enable MISRA rule (6.3) checking. */
+{
+  /* Write your local variable definition here */
+ int i=0,j=0;
+
+  /*** Processor Expert internal initialization. DON'T REMOVE THIS CODE!!! ***/
+  PE_low_level_init();
+  /*** End of Processor Expert internal initialization.                    ***/
+
+  /* Write your code here */
+  /* For example: for(;;) { } */
+	while(1)
+	{  		 
+		if(Bit1_GetVal()==0)
+		{		
+			i=i+1;
+			if(i==10)
+			{
+				j=j+1;
+				i=0;
+			}		
+			print(j,i);		 
+		}
+		else if(Bit2_GetVal()==0)
+		{
+			i=i-1;
+			if(i==-1)
+			{
+				i=9;
+				j=j-1;
+			}
+			print(j,i);		 
+		}
+		else
+		{	
+			print(j,i);
+		}
+	}
+  /*** Don't write any code pass this line, or it will be deleted during code generation. ***/
+  /*** RTOS startup code. Macro PEX_RTOS_START is defined by the RTOS component. DON'T MODIFY THIS CODE!!! ***/
+  #ifdef PEX_RTOS_START
+    PEX_RTOS_START();                  /* Startup of the selected RTOS. Macro is defined by the RTOS component. */
+  #endif
+  /*** End of RTOS startup code.  ***/
+  /*** Processor Expert end of main routine. DON'T MODIFY THIS CODE!!! ***/
+  for(;;){}
+  /*** Processor Expert end of main routine. DON'T WRITE CODE BELOW!!! ***/
+} /*** End of main routine. DO NOT MODIFY THIS TEXT!!! ***/
+
+
+/* END ProcessorExpert */
+/*!
+** @}
+*/
+/*
+** ###################################################################
+**
+**     This file was created by Processor Expert 10.3 [05.08]
+**     for the Freescale Kinetis series of microcontrollers.
+**
+** ###################################################################
+*/
+void print(int j,int i)
+{
+	 char ca[]={0xc0,0xf9,0xa4,0xb0,0x99,0x92,0x82,0xf8,0x80,0x90},cc[]={0x3f,0x06,0x5b,0x4f,0x66,0x6d,0x7d,0x07,0x7f,0x6f};
+	 int l;
+	 Byte1_PutVal(ca[j]);
+	 Byte2_PutVal(cc[i]);	
+	 for(l=0;l<0x1ffff;l++);
+}
